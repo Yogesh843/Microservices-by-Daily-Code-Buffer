21b408e0a5d1936bef9af399da98877d59c71a40
Yogesh Sharma <yogesh.sharma.1996.yks@gmail.com>
2024-08-01 16:56:28 +0530

    clone and pushed initial projects

---
+++ Department-Service/src/main/java/com/dailycodebuffer/department/controller/DepartmentController.java
@@ -0,0 +1,15 @@
+package com.dailycodebuffer.department.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+
+}
---
+++ Department-Service/src/main/java/com/dailycodebuffer/department/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.department.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+}
